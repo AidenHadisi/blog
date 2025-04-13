e2ee75c8c5b59048037887a1e45a315cde6ce8f1
Aiden Hadisi <so.hadisi@gmail.com>
2025-04-12 20:46:07 -0700

    updates

--- src/utils/getUniqueTags.ts
+++
@@ -1,8 +0,0 @@
-import type { CollectionEntry } from "astro:content";
-
-export const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
-  const tags: string[] = posts.flatMap((post) => post.data.tags);
-
-  // Get unique tags
-  return [...new Set(tags)].sort((a, b) => a.localeCompare(b));
-};
